314f9f056ddd863e05527108159d880a3a6c0242
Max <50644368+StealthSov@users.noreply.github.com>
2024-10-21 18:29:14 -0400

    In progress setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
+
+
   return (
-    <div>
-      
+    <div className="absolute inset-2 flex items-center justify-center flex-col">
+      <div className="text-5xl flex gap-3 items-center">{"<"}Maxwell Reinold <span className="text-3xl text-muted">inProgress={"{true}"}</span>{"/>"}</div>
     </div>
   );
 }
