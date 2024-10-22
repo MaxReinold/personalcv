e0c754b73377192bdeb8284fbed61b5d591a05e1
Max <50644368+StealthSov@users.noreply.github.com>
2024-10-22 13:37:41 -0400

    Changed Page, testing redeployment.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 
   return (
     <div className="absolute inset-2 flex items-center justify-center flex-col">
-      <div className="text-5xl flex gap-3 items-center">{"<"}Maxwell Reinold <span className="text-3xl text-muted">inProgress={"{true}"}</span>{"/>"}</div>
+      <div className="text-5xl flex gap-3 items-center">{"<"}Portfolio <span className="text-xl text-muted flex flex-col"><span>inProgress={"{true}"}</span><span>for={'{"Maxwell Reinold"}'}</span> </span>{"/>"}</div>
     </div>
   );
 }
